695b516b2fa3626c12fbd5cf3bd2ea1bd504ce66
Marshall Mickelson <me@earthcolonist.com>
2016-09-29 08:48:59 -0600

    Moved humanize methods to base class

--- lib/humanize/bytes.rb
+++ lib/humanize/bytes.rb
@@ -1,6 +1,6 @@
 module Humanize
   class Bytes
-    VERSION = '2.3.0'
+    VERSION = '2.3.1'
     BASE_MEASURE = 1024.0
     KILO = BASE_MEASURE
     MEGA = KILO * BASE_MEASURE
@@ -21,6 +21,32 @@ module Humanize
 
     def label
       "Bytes"
+    end
+    
+    def to_human_s
+      h = to_human
+      h.to_s(:decimal_digits => 2) + " #{h.label}"
+    end
+
+    def to_human
+      if @value >= YOTTA
+        return to_y
+      elsif @value >= ZETTA
+        return to_z
+      elsif @value >= EXA
+        return to_e
+      elsif @value >= PETA
+        return to_p
+      elsif @value >= TERA
+        return to_t
+      elsif @value >= GIGA
+        return to_g
+      elsif @value >= MEGA
+        return to_m
+      elsif @value >= KILO
+        return to_k
+      end
+      return self
     end
 
     def to_s(options = {})
